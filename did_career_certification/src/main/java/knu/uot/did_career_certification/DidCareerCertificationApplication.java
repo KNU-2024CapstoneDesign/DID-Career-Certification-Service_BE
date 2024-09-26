2f8ea207ee76ee3420ef89da1d1dcf20036d575b
chris0825 <chris00825@naver.com>
2024-09-26 11:38:36 +0900

    Initial commit

---
+++ did_career_certification/src/main/java/knu/uot/did_career_certification/DidCareerCertificationApplication.java
@@ -0,0 +1,13 @@
+package knu.uot.did_career_certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidCareerCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidCareerCertificationApplication.class, args);
+	}
+
+}
---
+++ did_career_certification/src/test/java/knu/uot/did_career_certification/DidCareerCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.uot.did_career_certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidCareerCertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
