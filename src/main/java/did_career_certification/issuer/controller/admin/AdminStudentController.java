5c6f490e4cb3e91b9af29fa1c98be47aa245eae0
Seunghyun Ko <chris00825@naver.com>
2024-11-06 13:25:35 +0900

    feat: student register page

--- src/main/java/did_career_certification/issuer/controller/admin/AdminStudentController.java
+++ src/main/java/did_career_certification/issuer/controller/admin/AdminStudentController.java
@@ -1,5 +1,10 @@
 package did_career_certification.issuer.controller.admin;
 
+import did_career_certification.issuer.entity.Student;
+import did_career_certification.issuer.enums.AcademicStatus;
+import did_career_certification.issuer.enums.College;
+import did_career_certification.issuer.enums.Degree;
+import did_career_certification.issuer.enums.Major;
 import did_career_certification.issuer.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin/issuer")
+@RequestMapping("/admin/issuer/students")
 @RequiredArgsConstructor
 public class AdminStudentController {
 
@@ -19,5 +24,15 @@ public class AdminStudentController {
     public String getStudentList(Model model, Pageable pageable) {
         model.addAttribute("studentList", studentService.findAll(pageable));
         return "student-management-page";
+    }
+
+    @GetMapping("/register")
+    public String registerStudent(Model model) {
+        model.addAttribute("student", new Student());
+        model.addAttribute("colleges", College.values());
+        model.addAttribute("majors", Major.values());
+        model.addAttribute("degrees", Degree.values());
+        model.addAttribute("academicStatus", AcademicStatus.values());
+        return "student-register-form";
     }
 }
