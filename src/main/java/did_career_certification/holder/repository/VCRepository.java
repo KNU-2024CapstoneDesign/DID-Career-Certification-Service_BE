3dcd4f4a30a24e1b85a908b050128ecb65885a26
Seunghyun Ko <chris00825@naver.com>
2024-11-08 22:53:33 +0900

    fix: bean registration conflict for VCRepository

--- src/main/java/did_career_certification/holder/repository/VCRepository.java
+++ src/main/java/did_career_certification/holder/repository/VCRepository.java
@@ -4,7 +4,9 @@ import did_career_certification.holder.entity.Holder;
 import did_career_certification.holder.entity.VC;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("HolderVCRepository")
 public interface VCRepository extends JpaRepository<VC, Long> {
     List<VC> findAllByHolder(Holder holder);
 }
--- src/main/java/did_career_certification/verifier/repository/VCRepository.java
+++ src/main/java/did_career_certification/verifier/repository/VCRepository.java
@@ -4,7 +4,9 @@ import did_career_certification.verifier.entity.VC;
 import did_career_certification.verifier.entity.VP;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("VerifierVCRepository")
 public interface VCRepository extends JpaRepository<VC, Long> {
 
     List<VC> findAllByVp(VP vp);
